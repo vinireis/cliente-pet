b5ac2d195d223d7d16ce3902e135e01644de0df5
Vinicius Reis <vinicius.reis@msn.com>
2022-07-21 21:11:30 -0300

    implementa request e response

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteResponse.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteResponse.java
@@ -1,5 +1,10 @@
 package br.com.petz.clientepet.cliente.application.api;
 
-public class ClienteResponse {
+import java.util.UUID;
 
+import lombok.Value;
+
+@Value
+public class ClienteResponse {
+	private UUID idCliente;
 }
