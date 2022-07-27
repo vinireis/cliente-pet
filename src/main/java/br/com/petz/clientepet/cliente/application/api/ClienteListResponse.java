c7f57a8832af10ad980b736eec85276670a9cc97
Vinicius Reis <vinicius.reis@msn.com>
2022-07-26 21:54:40 -0300

    implementa getTodos

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteListResponse.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteListResponse.java
@@ -2,9 +2,12 @@ package br.com.petz.clientepet.cliente.application.api;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import br.com.petz.clientepet.cliente.domain.Cliente;
+import lombok.Value;
 
+@Value
 public class ClienteListResponse {
 	private UUID idCliente;
 	private String nomeCompleto;
@@ -13,6 +16,16 @@ public class ClienteListResponse {
 	private String celular;
 	
 	public static List<ClienteListResponse> converte(List<Cliente> clientes) {
-		return null;
+		return clientes.stream()
+				.map(ClienteListResponse::new)
+				.collect(Collectors.toList());
+	}
+
+	private ClienteListResponse(Cliente cliente) {
+		this.idCliente = cliente.getIdCliente();
+		this.nomeCompleto = cliente.getNomeCompleto();
+		this.cpf = cliente.getCpf();
+		this.email = cliente.getEmail();
+		this.celular = cliente.getCelular();
 	}
 }
--- src/main/java/br/com/petz/clientepet/cliente/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/petz/clientepet/cliente/infra/ClienteInfraRepository.java
@@ -26,7 +26,8 @@ public class ClienteInfraRepository implements ClienteRepository {
 	@Override
 	public List<Cliente> buscaTodosClientes() {
 		log.info("[inicia] ClienteInfraRepository - buscaTodosClientes");
+		List<Cliente> todosClientes = clienteSprintDataJPARepository.findAll();
 		log.info("[finaliza] ClienteInfraRepository - buscaTodosClientes");
-		return null;
+		return todosClientes;
 	}
 }
