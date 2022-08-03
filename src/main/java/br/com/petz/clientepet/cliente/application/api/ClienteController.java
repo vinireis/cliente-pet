19378a6a82dc59f82c1e6960f3e3fd7a078d6d32
Vinicius Reis <vinicius.reis@msn.com>
2022-08-03 20:38:53 -0300

    implementa camada API

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
@@ -39,4 +39,12 @@ public class ClienteController implements ClienteAPI {
 		log.info("[finaliza] ClienteController - getClienteAtravesId");
 		return clienteDetalhado;
 	}
+
+	@Override
+	public void deletaClienteAtravesId(UUID idCliente) {
+		log.info("[inicia] ClienteController - deletaClienteAtravesId");
+		log.info("[idCliente] {}", idCliente);
+		log.info("[finaliza] ClienteController - deletaClienteAtravesId");
+		
+	}
 }
