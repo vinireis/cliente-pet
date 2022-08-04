5571e44049882bb866925096ac4e14ebe9d70d12
Vinicius Reis <vinicius.reis@msn.com>
2022-08-03 21:25:58 -0300

    implementa fluxo de alteracao camaada API

---
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteAlteracaoRequest.java
@@ -0,0 +1,23 @@
+package br.com.petz.clientepet.cliente.application.api;
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import br.com.petz.clientepet.cliente.domain.Sexo;
+import lombok.Value;
+
+@Value
+public class ClienteAlteracaoRequest {
+	@NotBlank
+	private String nomeCompleto;
+	@NotBlank
+	private String celular;
+	private String telefone;
+	private Sexo sexo;
+	@NotNull
+	private LocalDate dataNascimento;
+	@NotNull
+	private Boolean aceitaTermos;
+}
