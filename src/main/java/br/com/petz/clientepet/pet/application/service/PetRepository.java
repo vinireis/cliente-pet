5104384aaf28bc8d0e32ef441c71ef1e97612651
Vinicius Reis <vinicius.reis@msn.com>
2022-08-11 05:11:09 -0300

    implementa delete pet camada de service e repository

--- src/main/java/br/com/petz/clientepet/pet/application/service/PetRepository.java
+++ src/main/java/br/com/petz/clientepet/pet/application/service/PetRepository.java
@@ -9,4 +9,5 @@ public interface PetRepository {
 	Pet salvaPet(Pet pet);
 	List<Pet> buscaPetsDoClienteComID(UUID idCliente);
 	Pet buscaPetPeloId(UUID idPet);
+	void deletaPet(Pet pet);
 }
--- src/main/java/br/com/petz/clientepet/pet/infra/PetInfraRepository.java
+++ src/main/java/br/com/petz/clientepet/pet/infra/PetInfraRepository.java
@@ -42,4 +42,11 @@ public class PetInfraRepository implements PetRepository {
 		log.info("[finish] PetInfraRepository - buscaPetPeloId");
 		return pet;
 	}
+
+	@Override
+	public void deletaPet(Pet pet) {
+		log.info("[start] PetInfraRepository - deletaPet");
+		petSpringDataJPARepository.delete(pet);
+		log.info("[finish] PetInfraRepository - deletaPet");
+	}
 }
